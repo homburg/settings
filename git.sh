c32de0debe4b8ea7a8d9d1bf0340b85c36bc5e73
Thomas B Homburg <tbh@ordbogen.com>
2013-06-06 10:05:52 +0200

    Added user info

--- git.sh
+++ git.sh
@@ -4,3 +4,6 @@
 
 git config --global color.ui true
 git config --global --add alias.root '!pwd'
+
+git config --global user.name "Thomas B Homburg"
+git config --global user.email "thomas@homburg.dk"

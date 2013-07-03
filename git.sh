6f31ed763a1702c5d4276f66219d0967f5f9aeec
Thomas B Homburg <thomas@homburg.dk>
2013-07-03 23:11:46 +0200

    Added aliases

--- git.sh
+++ git.sh
@@ -5,6 +5,10 @@
 git config --global color.ui true
 git config --global --add alias.root '!pwd'
 git config --global --add alias.ci "commit"
+git config --global --add alias.co "checkout"
+git config --global --add alias.cl "clone"
+git config --global --add alias.st "status"
+git config --global --add alias.s "status"
 
 git config --global user.name "Thomas B Homburg"
 git config --global user.email "thomas@homburg.dk"

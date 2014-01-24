363c4e0576970920eadff12d50b97dd8844458be
Thomas B Homburg <thomas@homburg.dk>
2014-01-24 13:13:35 +0100

    Support makefile targets with \:

---
+++ fish/functions/__fish_print_make_targets.fish
@@ -0,0 +1,12 @@
+function __fish_print_make_targets
+	set files Makefile makefile GNUmakefile
+	# Some seds (e.g. on Mac OS X), don't support \n in the RHS
+	# Use a literal newline instead
+	# http://sed.sourceforge.net/sedfaq4.html#s4.1
+	sgrep -h -E '^[^#%=$[:space:]][^#%=$]*:([^=]|$)' $files ^/dev/null | \
+		sed "s/[\]:/__fish_escaped_colon/g" | \
+		cut -d ":" -f 1 | \
+		sed -e 's/^ *//;s/ *$//;s/  */\\
+		/g' ^/dev/null | \
+		sed "s/__fish_escaped_colon/:/g"
+end

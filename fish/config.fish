0551803495337ed78a7045c57b834ca29917e8df
Thomas B Homburg <thomas@homburg.dk>
2014-01-24 13:42:36 +0100

    Move ubuntu-specific settings to config.fish.ubuntu

--- fish/config.fish
+++ fish/config.fish
@@ -8,21 +8,6 @@ end
 
 . ~/code/settings/fish/virtualfish/virtual.fish
 
-## paths
-
-# This should work according to https://github.com/fish-shell/fish-shell/issues/527
-# But it does not
-# set -U fish_user_paths ~/bin
-
-# This works
-# ruby
-if test -d /var/lib/gems/1.9/bin
-	set -x PATH /var/lib/gems/1.9/bin $PATH
-end
-if test -d /var/Lib/gems/1.8/bin
-	set -x PATH /var/lib/gems/1.8/bin $PATH
-end
-
 # Local php packages
 if test -d ~/code/php/lib/bin
 	set -x PATH ~/code/php/lib/bin $PATH
@@ -35,8 +20,6 @@ end
 if test -d ~/code/go/bin
 	set -x PATH ~/code/go/bin $PATH
 end
-
-set -x PATH ~/bin $PATH /sbin
 
 # Editor
 set -x EDITOR vi

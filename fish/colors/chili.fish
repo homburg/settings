d5b45e489570f8ef0929c313d5a56bf4c80515bf
Thomas B Homburg <thomas@homburg.dk>
2014-02-04 09:07:14 +0100

    Add color scheme

---
+++ fish/colors/chili.fish
@@ -0,0 +1,7 @@
+set -gx fish_color_hostname F2C695
+set -gx fish_color_user 670D0F -o
+set -gx fish_color_cwd D14E2D
+set -gx fish_color_git_branch B42E22 -o
+set -gx fish_color_git_branch_wrap 444
+set -gx fish_color_prompt_base black
+set -gx fish_color_prompt_prompt black
